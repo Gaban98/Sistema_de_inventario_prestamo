7ca75901373928e77a9671a0343ecb93c6592567
Gaban98 <csrivera00@misena.edu.co>
2023-08-29 11:48:20 -0500

    modificacion de los archivos, activacion de modal para crear en categorias con las views ty urls editadas

--- inventario/views.py
+++ inventario/views.py
@@ -3,78 +3,86 @@ from django.urls import reverse_lazy
 from django.views.generic import *
 from .models import *
 from .form import *
+from django.shortcuts import render, redirect
 
 ###################     Para AGREGAR    ######################
 
 class CrearSede(CreateView):
     model = Sede
     fields = ['nombre', 'direccion', 'telefono', 'jefe_directo']
-    template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_crear'
+    template_name = 'SedeCategoria/crear_sede.html'
+    success_url = 'SedeCategoria/listar_sede_categoria'
 
 class CrearCategoria(CreateView):
     model = Categoria
     fields = ['nombre']
-    template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_crear'
+    template_name = 'SedeCategoria/listar_categoria.html'
+    success_url = reverse_lazy('inventario:listado_categorias')
 
 class CrearElemento(CreateView):
     model = Elemento
     fields = ['nombre', 'descripcion', 'estado', 'disponibilidad', 'sede', 'categoria']
-    template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_crear'
+    template_name = 'crear_elemento.html'
+    success_url = 'inventario/listar_elemento'
 
 ###################     Para EDITAR     ######################
 
 class ActualizarSede(UpdateView):
     model = Sede
     fields = ['nombre', 'direccion', 'telefono', 'jefe_directo']
-    template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_editar'
+    template_name = 'SedeCategoria/editar_sede.html'
+    success_url = 'SedeCategoria/listar_sede_categoria'
+
 
 class ActualizarCategoria(UpdateView):
     model = Categoria
     fields = ['nombre']
-    template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_editar'
+    template_name = 'SedeCategoria/editar_categoria.html'
+    success_url = reverse_lazy('inventario:listado_categorias')
 
 class ActualizarElemento(UpdateView):
     model = Elemento
     fields = ['nombre', 'descripcion', 'estado', 'disponibilidad', 'sede', 'categoria']
     template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_editar'
+    success_url = 'inventario/listar_elemento'
 
 ###################     Para ELIMINAR   ######################
 
 class EliminarSede(DeleteView):
     model = Sede
     template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_eliminar'
+    success_url = '/dSedeCategoria/listar_sede_categoria'
 
 class EliminarCategoria(DeleteView):
     model = Categoria
-    template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_eliminar'
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return redirect('inventario:listado_categorias')
 
 class EliminarElemento(DeleteView):
     model = Elemento
     template_name = 'nombre_del_template.html'
-    success_url = '/donde_redirigir_despues_de_eliminar'
+    success_url = 'inventario/listar_elemento'
 
 ###################     Para LISTAR     ######################
 
 
 class ListadoSede(ListView):
     model = Sede
-    template_name = 'nombre_del_template.html'
+    template_name = 'SedeCategoria/listar_sede.html'
     context_object_name = 'sedes'
 
 class ListadoCategoria(ListView):
     model = Categoria
-    template_name = 'nombre_del_template.html'
+    template_name = 'SedeCategoria/listar_categoria.html'
     context_object_name = 'categorias'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_categoria'] = CategoriaForm()  # Agregar el formulario al contexto
+        return context
 
 class ListadoElemento(ListView):
     model = Elemento
-    template_name = 'nombre_del_template.html'
+    template_name = 'SedeCategoria/listar_elemento.html'
     context_object_name = 'elementos'
--- static/js/logica.js
+++ static/js/logica.js
@@ -1,43 +1,9 @@
-function updateClock() {
-    const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes();
-
-    const timeString = hours + ":" + minutes ;
-    document.getElementById("current-time").innerText = timeString;
-}
-
-// Actualizar la hora cada segundo
-setInterval(updateClock, 1000);
-
-function mostrarFormulario(tipo) {
-    if (tipo === 'sede') {
-        document.getElementById('formularioSede').style.display = 'block';
-        document.getElementById('formularioCategoria').style.display = 'none';
-    } else if (tipo === 'categoria') {
-        document.getElementById('formularioSede').style.display = 'none';
-        document.getElementById('formularioCategoria').style.display = 'block';
-    }
-}
-
-
-$(document).ready(function() {
-    // Cuando se hace clic en "Agregar Sede"
-    $("#sede-modal-trigger").click(function() {
-        loadModalContent("{% url 'inventario:sede-create' %}", "Agregar Sede");
+$(document).on('click', '.editar-categoria', function(){
+    var id = $(this).data('id');
+    $.ajax({
+        url: "/actualizar_categoria/" + id,  // Asegúrate de que esta URL dirige a la vista de edición de la categoría
+        success: function(data){
+            $("#editarCategoriaModal .modal-body").html(data);
+        }
     });
-
-    // Cuando se hace clic en "Agregar Categoría"
-    $("#categoria-modal-trigger").click(function() {
-        loadModalContent("{% url 'inventario:categoria-create' %}", "Agregar Categoría");
-    });
-
-    // Función para cargar contenido en el modal
-    function loadModalContent(url, title) {
-        $.get(url, function(data) {
-            $(".modal-body").html(data);
-            $(".modal-title").html(title);
-            $("#myModal").modal("show");
-        });
-    }
 });
